0e6cd4ccc4712f6c5f364ada30a08d43e01efc4f
Vadim Hmys <vadimhmyz@gmail.com>
2024-03-11 23:39:53 +0300

    counter witch ref doesn't work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { useRef, useState } from "react";
 
 export default function Counter() {
-  const [count, setCount] = useState(0);
+  let countRef = useRef(0);
 
   function handleClick() {
-    setCount(count + 1);
+    countRef.current = countRef.current + 1;
   }
  
   return (
     <button onClick={handleClick}>
-      You clicked {count} times!
+      You clicked {countRef.current} times!
     </button>
   );
 }
