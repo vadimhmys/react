77190721af6cab3f04d7b29dd7d89f5f82d51804
Vadim Hmys <vadimhmyz@gmail.com>
2024-03-12 00:07:41 +0300

    start ref task 2

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
-import { useRef, useState } from 'react';
+import { useRef } from 'react';
 
-export default function Chat() {
-  const [text, setText] = useState('');
-  const [isSending, setIsSending] = useState(false);
-  const timeoutID = useRef(null);
-
-  function handleSend() {
-    setIsSending(true);
-    timeoutID.current = setTimeout(() => {
-      alert('Sent!');
-      setIsSending(false);
-    }, 3000);
-  }
-
-  function handleUndo() {
-    setIsSending(false);
-    clearTimeout(timeoutID.current);
-  }
+export default function Toggle() {
+  const isOnRef = useRef(false);
 
   return (
-    <>
-      <input
-        disabled={isSending}
-        value={text}
-        onChange={e => setText(e.target.value)}
-      />
-      <button
-        disabled={isSending}
-        onClick={handleSend}>
-        {isSending ? 'Sending...' : 'Send'}
-      </button>
-      {isSending &&
-        <button onClick={handleUndo}>
-          Undo
-        </button>
-      }
-    </>
+    <button onClick={() => {
+      isOnRef.current = !isOnRef.current;
+    }}>
+      {isOnRef.current ? 'On' : 'Off'}
+    </button>
   );
 }
