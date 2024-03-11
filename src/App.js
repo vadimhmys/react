45f60d4c79ced4417fc45dabc90c3f687813bc91
Vadim Hmys <vadimhmyz@gmail.com>
2024-03-12 00:14:11 +0300

    start refs task 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import { useState } from 'react';
+let timeoutID;
 
-export default function Toggle() {
-  const [isOn, setIsOn] = useState(false);
-
+function DebouncedButton({ onClick, children }) {
   return (
-    <button onClick={() => setIsOn(!isOn)}>
-      {isOn ? 'On' : 'Off'}
+    <button onClick={() => {
+      clearTimeout(timeoutID);
+      timeoutID = setTimeout(() => {
+        onClick();
+      }, 1000);
+    }}>
+      {children}
     </button>
   );
 }
+
+export default function Dashboard() {
+  return (
+    <>
+      <DebouncedButton
+        onClick={() => alert('Spaceship launched!')}
+      >
+        Launch the spaceship
+      </DebouncedButton>
+      <DebouncedButton
+        onClick={() => alert('Soup boiled!')}
+      >
+        Boil the soup
+      </DebouncedButton>
+      <DebouncedButton
+        onClick={() => alert('Lullaby sung!')}
+      >
+        Sing a lullaby
+      </DebouncedButton>
+    </>
+  )
+}
+
