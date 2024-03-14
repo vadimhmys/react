8d4a4c128906ab7ee505e4cca4cfd8c60600d5b5
Vadim Hmys <vadimhmyz@gmail.com>
2024-03-14 22:02:45 +0300

    problem last todo in todoList

--- src/App.js
+++ src/App.js
@@ -1,31 +1,46 @@
-import { forwardRef, useImperativeHandle, useRef } from "react";
+import { useRef, useState } from "react";
 
-const MyInput = forwardRef((props, ref) => {
-  const realInputRef = useRef(null);
-  useImperativeHandle(ref, () => ({
-    focus() {
-      realInputRef.current.focus();
-    }
-  }));
-  return <input {...props} ref={realInputRef}/>
-});
+export default function TodoList() {
+  const listRef  = useRef(null);
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState(
+    initialTodos
+  );
 
-export default function MyForm() {
-  const inputRef = useRef(null);
-
-  function handleClick() {
-    inputRef.current.focus();
-    //inputRef.current.style.display = "none";
+  function handleAdd() {
+    const newTodo = {id: nextId++, text: text};
+    setText('');
+    setTodos([...todos, newTodo]);
+    listRef.current.lastChild.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest'
+    });
   }
 
   return (
     <>
-      <MyInput 
-        type="text" 
-        placeholder="Enter your message..."
-        ref={inputRef}
-        />
-      <button onClick={handleClick}>Click to focus</button>
+      <button onClick={handleAdd}>
+        Add
+      </button>
+      <input
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <ul ref={listRef}>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
     </>
   );
+
+}
+
+let nextId = 0;
+let initialTodos = [];
+for (let i = 0; i < 20; i++) {
+  initialTodos.push({
+    id: nextId++,
+    text: 'Todo #' + (i + 1)
+  });
 }
