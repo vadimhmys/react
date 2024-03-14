0cc2369531e21542da7b5e7b4b2fa3b259b0fdde
Vadim Hmys <vadimhmyz@gmail.com>
2024-03-14 21:11:50 +0300

    use forwardRef

--- src/App.js
+++ src/App.js
@@ -1,70 +1,25 @@
-import { useRef } from "react";
+import { forwardRef, useRef } from "react";
 
+const MyInput = forwardRef((props, ref) => {
+  return <input {...props} ref={ref}/>
+});
 
-export default function CatFriends() {
-  const itemsRef = useRef(null);
-  
+export default function MyForm() {
+  const inputRef = useRef(null);
 
-  function scrollToId(itemId) {
-    const map = getMap();
-    const node = map.get(itemId);
-    node.scrollIntoView({
-      behavior: "smooth",
-      block: "nearest",
-      inline: "center"
-    });
-  }
-
-  function getMap() {
-    if (!itemsRef.current) {
-      itemsRef.current = new Map();
-    }
-    return itemsRef.current;
+  function handleClick() {
+    inputRef.current.focus();
+    inputRef.current.style.display = "none";
   }
 
   return (
     <>
-      <nav>
-        <button onClick={() => scrollToId(0)}>
-          Tom
-        </button>
-        <button onClick={() => scrollToId(6)}>
-          Maru
-        </button>
-        <button onClick={() => scrollToId(9)}>
-          Jellylorum
-        </button>
-      </nav>
-      <div>
-        <ul>
-          {catList.map(cat => (
-            <li
-              key={cat.id}
-              ref={(node) => {
-                const map = getMap();
-                if (node ) {
-                  map.set(cat.id, node)
-                } else {
-                  map.delete(cat.id);
-                }
-              }}
-            >
-              <img
-                src={cat.imageUrl}
-                alt={'Cat #' + cat.id}
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
+      <MyInput 
+        type="text" 
+        placeholder="Enter your message..."
+        ref={inputRef}
+        />
+      <button onClick={handleClick}>Click to focus</button>
     </>
   );
-}
-
-const catList = [];
-for (let i = 0; i < 10; i++) {
-  catList.push({
-    id: i,
-    imageUrl: 'https://placekitten.com/250/200?image=' + i
-  });
 }
