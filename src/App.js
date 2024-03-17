5dc4a506fa220d7b77d838554c63b4e05768cff7
Vadim Hmys <vadimhmyz@gmail.com>
2024-03-17 11:27:15 +0300

    manipulation the DOM start task 2

--- src/App.js
+++ src/App.js
@@ -1,35 +1,12 @@
-import { useState, useRef } from 'react';
-
-export default function VideoPlayer() {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const videoRef = useRef(null);
-
-  function handleClick() {
-    const nextIsPlaying = !isPlaying;
-    setIsPlaying(nextIsPlaying);
-    if (nextIsPlaying) {
-      videoRef.current.play();
-    } else {
-      videoRef.current.pause();
-    }
-  }
-
+export default function Page() {
   return (
     <>
-      <button onClick={handleClick}>
-        {isPlaying ? 'Pause' : 'Play'}
-      </button>
-      <video 
-        ref={videoRef} 
-        width="250"
-        onPlay={() => setIsPlaying(true)}
-        onPause={() => setIsPlaying(false)}
-        >
-        <source
-          src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-          type="video/mp4"
-        />
-      </video>
+      <nav>
+        <button>Search</button>
+      </nav>
+      <input
+        placeholder="Looking for something?"
+      />
     </>
-  )
+  );
 }
