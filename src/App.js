42dc16d1595b1fd67be25dae74320d6db4aecfee
Vadim Hmys <vadimhmyz@gmail.com>
2024-03-17 11:30:07 +0300

    manipulation the DOM finish task 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { useRef } from "react";
+
 export default function Page() {
+  const inputRef = useRef(null);
+
+  function handleClick() {
+    inputRef.current.focus();
+  }
+
   return (
     <>
       <nav>
-        <button>Search</button>
+        <button onClick={handleClick}>Search</button>
       </nav>
       <input
+        ref={inputRef}
         placeholder="Looking for something?"
       />
     </>
