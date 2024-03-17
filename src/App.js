b8aabf7093065c84a278e8d80affaef88e4d32db
Vadim Hmys <vadimhmyz@gmail.com>
2024-03-17 11:31:19 +0300

    manipulation the DOM start task 3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
-import { useRef } from "react";
+import { useState } from 'react';
 
-export default function Page() {
-  const inputRef = useRef(null);
-
-  function handleClick() {
-    inputRef.current.focus();
-  }
-
+export default function CatFriends() {
+  const [index, setIndex] = useState(0);
   return (
     <>
       <nav>
-        <button onClick={handleClick}>Search</button>
+        <button onClick={() => {
+          if (index < catList.length - 1) {
+            setIndex(index + 1);
+          } else {
+            setIndex(0);
+          }
+        }}>
+          Next
+        </button>
       </nav>
-      <input
-        ref={inputRef}
-        placeholder="Looking for something?"
-      />
+      <div>
+        <ul>
+          {catList.map((cat, i) => (
+            <li key={cat.id}>
+              <img
+                className={
+                  index === i ?
+                    'active' :
+                    ''
+                }
+                src={cat.imageUrl}
+                alt={'Cat #' + cat.id}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
+
+const catList = [];
+for (let i = 0; i < 10; i++) {
+  catList.push({
+    id: i,
+    imageUrl: 'https://placekitten.com/250/200?image=' + i
+  });
+}
+
