9444c2b449ce1e60a99573fd5640e399512c0dce
Vadim Hmys <vadimhmyz@gmail.com>
2024-03-25 20:59:56 +0300

    visualize remounting effect

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect } from "react";
+import { createConnection } from "./chat";
 
-function VideoPlayer({src, isPlaying}) {
-  const ref = useRef(null);
-
+export default function ChatRoom() {
   useEffect(() => {
-    if (isPlaying) {
-      console.log('Calling video.play()');
-      ref.current.play();
-    } else {
-      console.log('Calling video.pause()');
-      ref.current.pause();
-    }
-  }, [isPlaying]);
-
-  return <video ref={ref} src={src} loop playsInline/>
-}
-
-export default function App() {
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [text, setText] = useState('');
-
-  return (
-    <>
-      <input value={text} onChange={e => setText(e.target.value)}/>
-      <button onClick={() => setIsPlaying(!isPlaying)}>
-        {isPlaying ? 'Pause': 'Play'}
-      </button>
-      <VideoPlayer
-        isPlaying={isPlaying}
-        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
-      />
-    </>
-  );
+    const connection = createConnection();
+    connection.connect();
+  }, []);
+  return <h1>Welcome to the chat!</h1>
 }
